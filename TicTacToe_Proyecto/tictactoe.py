ed0ac9af413902985d86c4f160951441ab3c9f08
Krlozz Ayala <k_arlos210@hotmail.com>
2022-12-13 15:18:37 -0500

    TicTacToe
    Proyecto I Bimestre

---
+++ TicTacToe_Proyecto/tictactoe.py
@@ -0,0 +1,243 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import random
+from copy import deepcopy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def minimax(board):
+    """
+    Rutorna la opcion optima
+
+    'X' maximisa, 'O' minimiza el puntaje
+    """
+
+    global actions_explored
+    actions_explored = 0
+
+    def max_player(board, best_min = 10):
+      """ 
+          maximiza el puntaje de x
+          usa el mejor min 
+          
+      """
+
+      global actions_explored
+
+      if terminal(board):
+        return (utility(board), None)
+
+      value = -10
+      best_action = None
+
+
+      action_set = actions(board)
+
+      while len(action_set) > 0:
+        action = random.choice(tuple(action_set))
+        action_set.remove(action)
+
+        if best_min <= value:
+          break
+
+        actions_explored += 1
+        min_player_result = min_player(result(board, action), value)
+        if min_player_result[0] > value:
+          best_action = action
+          value = min_player_result[0]
+
+      return (value, best_action)
+
+
+    def min_player(board, best_max = -10):
+      """ Helper function to minimise score for 'O' player """
+
+      global actions_explored
+
+      # If the game is over, return board value
+      if terminal(board):
+        return (utility(board), None)
+
+      # Else pick the action giving the min value when max_player plays optimally
+      value = 10
+      best_action = None
+
+      # Get set of actions and then select a random one until list is empty:
+      action_set = actions(board)
+
+      while len(action_set) > 0:
+        action = random.choice(tuple(action_set))
+        action_set.remove(action)
+
+        # A-B Pruning skips calls to max_player if higher result already found:
+        if best_max >= value:
+          break
+
+        actions_explored += 1
+        max_player_result = max_player(result(board, action), value)
+        if max_player_result[0] < value:
+          best_action = action
+          value = max_player_result[0]
+
+      return (value, best_action)
+
+
+    # If the board is terminal, return None:
+    if terminal(board):
+      return None
+
+    if player(board) == 'X':
+      print('AI is exploring possible actions...')
+      best_move = max_player(board)[1]
+      print('Actions explored by AI: ', actions_explored)
+      return best_move
+    else:
+      print('AI is exploring possible actions...')
+      best_move = min_player(board)[1]
+      print('Actions explored by AI: ', actions_explored)
+      return best_move
+  
+    
+  
+    
+  
+
+def initial_state():
+  """
+  Returns starting state of the board.
+  """
+  return [[EMPTY, EMPTY, EMPTY],
+          [EMPTY, EMPTY, EMPTY],
+          [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    
+    # Scan board for entries and determine next player:
+
+    X_count = 0
+    O_count = 0
+    EMPTY_count = 0
+
+    for row in board:
+      X_count += row.count(X)
+      O_count += row.count(O)
+      EMPTY_count += row.count(EMPTY)
+
+    # If X has more squares than O, its O's turn:
+    if X_count > O_count:
+      return O
+
+    # Otherwise it is X's turn:
+    else:
+      return X
+
+
+def actions(board):
+
+
+    moves = set()
+
+    for i in range(3):
+      for j in range(3):
+        if board[i][j] == EMPTY:
+          moves.add((i, j))
+
+    return moves
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+
+    i = action[0]
+    j = action[1]
+
+    # Verifica movimiento invalido:
+    if i not in [0, 1, 2] or j not in [0, 1, 2]:
+      raise InvalidActionError(action, board, 'Result function given an invalid board position for action: ')
+    elif board[i][j] != EMPTY:
+      raise InvalidActionError(action, board, 'Result function tried to perform invalid action on occupaied tile: ')
+
+    # hace una copia y actualiza el jugador que mueve actualmente:
+    board_copy = deepcopy(board)
+    board_copy[i][j] = player(board)
+
+    return board_copy
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+
+    # Check rows:
+    for row in board:
+      if row.count(X) == 3:
+        return X
+      if row.count(O) == 3:
+        return O
+
+    # Check columns:
+    for j in range(3):
+      column = ''
+      for i in range(3):
+        column += str(board[i][j])
+
+      if column == 'XXX':
+        return X
+      if column == 'OOO':
+        return O
+
+    # Check Diagonals:
+    diag1 = ''
+    diag2 = ''
+    j = 2
+
+    for i in range(3):
+      diag1 += str(board[i][i])
+      diag2 += str(board[i][j])
+      j -= 1
+
+    if diag1 == 'XXX' or diag2 == 'XXX':
+      return X
+    elif diag1 == 'OOO' or diag2 == 'OOO':
+      return O
+
+    # Otherwise no current winner, return None
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    # Game is over if it is a winning board or all tiles are full (no actions):
+
+    if winner(board) or not actions(board):
+      return True
+    else:
+      return False
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+
+    if winner(board) == 'X':
+      return 1
+    elif winner(board) == 'O':
+      return -1
+    else:
+      return 0
+
+actions_explored = 0
+
